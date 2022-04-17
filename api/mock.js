60dcd95212e809e0292dc0f033987c84f7e070b0
guillainbisimwa <guillainbisimwa@hotmail.fr>
2022-04-17 03:06:17 +0200

    Add Fake Services

--- api/mock.js
+++ api/mock.js
@@ -1,7 +1,7 @@
 import { faker } from '@faker-js/faker';
 import { sample } from 'lodash';
 
-const clients = [...Array(5)].map((_, index) => ({
+const clients = {
     name: faker.name.findName(), 
     phone: faker.phone.phoneNumber('+48 ## ### ## ##'),
     email: faker.internet.email(),
@@ -20,32 +20,36 @@ const clients = [...Array(5)].map((_, index) => ({
         'Gauteng',
         'Free State',
         'Eastern Cape',
-    ]),
-    created :"2022-04-16T22:00:44.824+00:0"
-}));
+    ])
+};
 
-const services = [...Array(5)].map((_, index) => ({
+const services = {
     name: faker.company.companyName(),
-    desc: faker.commerce.productDescription(),
+    desc:  faker.fake(' {{commerce.productDescription}}, {{commerce.productDescription}}, {{commerce.productDescription}}!'),
     status:1,
     cat: sample([
-        "620bc030937c13ae027f5337",
-        "620bc088937c13ae027f5339",
-        "620bc09c937c13ae027f533b",
-        "620bc0aa937c13ae027f533d",
-        "620bc0cb937c13ae027f533f",
-        "620bc0d2937c13ae027f5341",
-        "620bc0e7937c13ae027f5343",
-        "620bc0f3937c13ae027f5345",
-        "620bc0ff937c13ae027f5347",
-        "620bc10b937c13ae027f5349",
-        "620bc11f937c13ae027f534b",
-        "620bc130937c13ae027f534d",
-        "620bc13c937c13ae027f534f",
-        "620bc147937c13ae027f5351",
-        "620bc151937c13ae027f5353",
+        "6237174bd3f931b463735bf8",
+        "62371784d3f931b463735bfa",
+        "6237178fd3f931b463735bfc",
+        "6237179bd3f931b463735bfe",
+        "623717aad3f931b463735c00",
+        "623717b6d3f931b463735c02",
+        "623717c1d3f931b463735c04",
+        "623717ccd3f931b463735c06",
+        "623717d8d3f931b463735c08",
+        "623717e4d3f931b463735c0a",
+        "623717f8d3f931b463735c0c",
+        "62371804d3f931b463735c0e",
+        "6237180fd3f931b463735c10",
+        "62371819d3f931b463735c12",
+        "62371822d3f931b463735c14",
     ]),
-    id_user: "*************",
+    id_user: sample([
+        "625b52c878821117f621a77f",
+        "625b545878821117f621a79b",
+        "625b557278821117f621a7a1",
+        "625b55db78821117f621a7ae",
+    ]),
     phone: faker.phone.phoneNumber('+48 ## ### ## ##'),
     email: faker.internet.email(),
     address: faker.address.streetAddress(true),
@@ -74,44 +78,31 @@ const services = [...Array(5)].map((_, index) => ({
         'Open',
         'Close'
     ]),
-    services: sample([
+   services: sample([
+        faker.fake(' {{commerce.productDescription}}, {{commerce.productDescription}}, {{commerce.productDescription}}!'),
         faker.fake(' {{commerce.productDescription}}, {{commerce.productDescription}}!'),
-        faker.fake(' {{commerce.productDescription}}, {{commerce.productDescription}}!'),
-        faker.fake(' {{commerce.productDescription}}, {{commerce.productDescription}}!'),
+        faker.fake(' {{commerce.productDescription}}, {{commerce.productDescription}}, {{commerce.productDescription}}!'),
         faker.fake(' {{commerce.productDescription}}, {{commerce.productDescription}}!'),
     ]),
     images: [
-        sample([
-            faker.image.abstract(),
-            faker.image.fashion(),
-            faker.image.city()
+         sample([
+            "https://res.cloudinary.com/micity/image/upload/v1647780394/markus-spiske-g5ZIXjzRGds-unsplash_1_wsfwx3.jpg",
+           "https://res.cloudinary.com/micity/image/upload/v1647780406/sigmund-CwTfKH5edSk-unsplash_kouyqn.jpg",
+            "https://res.cloudinary.com/micity/image/upload/v1647780420/rod-long-2P_ifaetDm0-unsplash_h0wp9z.jpg"
         ]),
         sample([
-            faker.image.food(),
-            faker.image.transport(),
-            faker.image.people()
+           "https://res.cloudinary.com/micity/image/upload/v1647780860/toa-heftiba-bnoPZ9aTyWQ-unsplash_nxfpig.jpg",
+           "https://res.cloudinary.com/micity/image/upload/v1647781167/westwind-air-service-C0dSvpljHcI-unsplash_z971zm.jpg",
+           "https://res.cloudinary.com/micity/image/upload/v1647781176/cynthia-magana-mFPJXEQfb9M-unsplash_iequif.jpg"
         ]),
         sample([
-            faker.image.sports(),
-            faker.image.image(),
-            faker.image.nature()
+           "https://res.cloudinary.com/micity/image/upload/v1647781191/vaida-tamosauskaite-oJofV8dZd_w-unsplash_rvysux.jpg",
+            "https://res.cloudinary.com/micity/image/upload/v1647781258/redd-8n-3h1WkaQk-unsplash_q1hiec.jpg",
+            "https://res.cloudinary.com/micity/image/upload/v1647781354/carlos-lindner-53wcYH4IOig-unsplash_zi7pxb.jpg"
         ]),
     ],
-    created: "2022-04-16T22:02:05.704+00:00"
-}));
+};
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+export {
+    clients, services
+}

585363660d2749e335f15989099206f868fca2a8
guillainbisimwa <guillainbisimwa@hotmail.fr>
2022-04-24 21:51:24 +0200

    Add Terms of use

--- .expo/README.md
+++ .expo/README.md
@@ -2,11 +2,13 @@
 
 The ".expo" folder is created when an Expo project is started using "expo start" command.
 
-> What do the files contain?
+> What does the "packager-info.json" file contain?
 
-- "devices.json": contains information about devices that have recently opened this project. This is used to populate the "Development sessions" list in your development builds.
-- "packager-info.json": contains port numbers and process PIDs that are used to serve the application to the mobile device/simulator.
-- "settings.json": contains the server configuration that is used to serve the application manifest.
+The "packager-info.json" file contains port numbers and process PIDs that are used to serve the application to the mobile device/simulator.
+
+> What does the "settings.json" file contain?
+
+The "settings.json" file contains the server configuration that is used to serve the application manifest.
 
 > Should I commit the ".expo" folder?
 
--- screens/Signup.js
+++ screens/Signup.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { Block, Button, Checkbox, Input, Select, Text } from "../components";
-import { Image, StyleSheet, Keyboard, ScrollView, ToastAndroid, ActivityIndicator } from "react-native";
+import { Image, View, StyleSheet, Keyboard,Modal, ScrollView, ToastAndroid, ActivityIndicator } from "react-native";
 import { Colors, Layout, mocks } from "../constants";
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import api from "../api/api";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 const Signup = ({navigation}) => {
 
@@ -17,6 +18,7 @@ const Signup = ({navigation}) => {
     const [city, setCity] = useState("");
     const [address, setAddress] = useState("");
     const [load, setLoad] = useState(false);
+    const [showTerms, setShowTerms] = useState(false);
 
     const [errors, setErrors] = useState({
         name :false,
@@ -166,6 +168,146 @@ const Signup = ({navigation}) => {
         }  
     }
 
+    const renderTermsService = () => {
+        return (
+          <Modal
+          animationType="slide"
+          visible={showTerms}
+          onRequestClose={() => {
+            setShowTerms(!showTerms);
+          }}>
+          
+            <Block  style={{flex:1}}>
+            <Block padding={Layout.padding}  style={{flex:1}}>
+              <Text h2 bold >
+                Privacy Policy
+              </Text>
+    
+              <ScrollView style={{ marginVertical: Layout.padding }}>
+                <Text
+                  caption
+                  gray
+                 // height={24}
+                  style={{ marginBottom: Layout.base }}
+                >
+                  1. Your use of the Service is at your sole risk. The service is
+                  provided on an "as is" and "as available" basis.
+                </Text>
+                <Text
+                  caption
+                  gray
+                 // height={24}
+                  style={{ marginBottom:  Layout.base  }}
+                >
+                  2. Support for Expo services is only available in English, via
+                  e-mail.
+                </Text>
+                <Text
+                  caption
+                  gray
+                 // height={24}
+                  style={{ marginBottom:  Layout.base  }}
+                >
+                  3. You understand that Expo uses third-party vendors and hosting
+                  partners to provide the necessary hardware, software, networking,
+                  storage, and related technology required to run the Service.
+                </Text>
+                <Text
+                  caption
+                  gray
+                 // height={24}
+                  style={{ marginBottom:  Layout.base  }}
+                >
+                  4. You must not modify, adapt or hack the Service or modify
+                  another website so as to falsely imply that it is associated with
+                  the Service, Expo, or any other Expo service.
+                </Text>
+                <Text
+                  caption
+                  gray
+                 // height={24}
+                  style={{ marginBottom:  Layout.base  }}
+                >
+                  5. You may use the Expo Pages static hosting service solely as
+                  permitted and intended to host your organization pages, personal
+                  pages, or project pages, and for no other purpose. You may not use
+                  Expo Pages in violation of Expo's trademark or other rights or in
+                  violation of applicable law. Expo reserves the right at all times
+                  to reclaim any Expo subdomain without liability to you.
+                </Text>
+                <Text
+                  caption
+                  gray
+                 // height={24}
+                  style={{ marginBottom:  Layout.base  }}
+                >
+                  6. You agree not to reproduce, duplicate, copy, sell, resell or
+                  exploit any portion of the Service, use of the Service, or access
+                  to the Service without the express written permission by Expo.
+                </Text>
+                <Text
+                  caption
+                  gray
+                 // height={24}
+                  style={{ marginBottom:  Layout.base  }}
+                >
+                  7. We may, but have no obligation to, remove Content and Accounts
+                  containing Content that we determine in our sole discretion are
+                  unlawful, offensive, threatening, libelous, defamatory,
+                  pornographic, obscene or otherwise objectionable or violates any
+                  party's intellectual property or these Terms of Service.
+                </Text>
+                <Text
+                  caption
+                  gray
+                 // height={24}
+                  style={{ marginBottom:  Layout.base  }}
+                >
+                  8. Verbal, physical, written or other abuse (including threats of
+                  abuse or retribution) of any Expo customer, employee, member, or
+                  officer will result in immediate account termination.
+                </Text>
+                <Text
+                  caption
+                  gray
+                 // height={24}
+                  style={{ marginBottom:  Layout.base  }}
+                >
+                  9. You understand that the technical processing and transmission
+                  of the Service, including your Content, may be transferred
+                  unencrypted and involve (a) transmissions over various networks;
+                  and (b) changes to conform and adapt to technical requirements of
+                  connecting networks or devices.
+                </Text>
+                <Text
+                  caption
+                  gray
+                 // height={24}
+                  style={{ marginBottom:  Layout.base  }}
+                >
+                  10. You must not upload, post, host, or transmit unsolicited
+                  e-mail, SMSs, or "spam" messages.
+                </Text>
+                
+              </ScrollView>
+             
+             
+              <Block >
+                <Button onPress={() => setShowTerms(!showTerms)}>
+                  <Text center caption white>
+                  I understand
+                  </Text>
+                </Button>
+              </Block>
+    
+            </Block>
+            
+            </Block>
+          </Modal>
+        );
+      }
+
+
     return <KeyboardAwareScrollView>
             <Block padding={50} color="white" animated>
             <Block flex={1} >
@@ -261,6 +403,11 @@ const Signup = ({navigation}) => {
                     }}
                     label="I accept the conditions of use."
                     />
+                    <Button onPress={() => setShowTerms(!showTerms)}>
+            <Text center caption white>
+              Conditions d'utilisations
+            </Text>
+          </Button>
                       
                 <Block flex={1} center>
                
@@ -275,8 +422,12 @@ const Signup = ({navigation}) => {
                         </Text>
                     </Button>
                 </Block>
+               
+                
             </Block>
+           
         </Block>
+        {renderTermsService()}
     </KeyboardAwareScrollView>
 };
 

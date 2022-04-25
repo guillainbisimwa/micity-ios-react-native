5b74dfc323f7872ec6ad603b9c91e132b790a1c3
guillainbisimwa <guillainbisimwa@hotmail.fr>
2022-04-25 12:07:05 +0200

    Transfer of Your Personal Data

--- screens/Signup.js
+++ screens/Signup.js
@@ -653,9 +653,31 @@ const Signup = ({navigation}) => {
                   style={{ marginBottom: Layout.base/2, fontWeight: "bold" }}
                 >
                 5. Transfer of Your Personal Data
-
                 </Text>
 
+                <Text
+                  caption
+                  gray
+                  style={{ marginBottom:  Layout.base/2  }}
+                >
+                  Your information, including Personal Data, is processed at the Company's operating offices and in any other places where the parties involved in the processing are located. It means that this information may be transferred to — and maintained on — computers located outside of Your state, province, country or other governmental jurisdiction where the data protection laws may differ than those from Your jurisdiction.
+                </Text>
+
+                <Text
+                  caption
+                  gray
+                  style={{ marginBottom:  Layout.base/2  }}
+                >
+                Your consent to this Privacy Policy followed by Your submission of such information represents Your agreement to that transfer.
+                </Text>
+
+                <Text
+                  caption
+                  gray
+                  style={{ marginBottom:  Layout.base/2  }}
+                >
+                The Company will take all steps reasonably necessary to ensure that Your data is treated securely and in accordance with this Privacy Policy and no transfer of Your Personal Data will take place to an organization or a country unless there are adequate controls in place including the security of Your data and other personal information.
+                </Text>
 
                 <Text
                   caption
